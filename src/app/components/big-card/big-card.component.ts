f2fb807386ff551dee636067feedb8399caf3189
dgomesdev <danilo.gomes@outlook.fr>
2024-02-10 13:42:57 -0400

    feat: added dynamic attributes to compoenents

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -8,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './big-card.component.scss'
 })
 export class BigCardComponent {
-
+  @Input()
+  cardPhoto: string = "";
+  @Input()
+  cardTitle: string = "";
+  @Input()
+  cardDescription: string = "";
 }
