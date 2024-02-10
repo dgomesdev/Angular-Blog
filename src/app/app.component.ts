e66eeddb407c2c306afd37ba2971412fe7dc5511
dgomesdev <danilo.gomes@outlook.fr>
2024-02-09 22:27:24 -0400

    feat: finished components creation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuTitleComponent } from "./components/menu-title/menu-title.component";
+import { HomeComponent } from "./pages/home/home.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, MenuTitleComponent, HomeComponent]
 })
 export class AppComponent {
   title = 'AngularBlog';
